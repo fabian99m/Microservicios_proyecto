56d6ae3d47bbaecb1512eb29e8f99086d846ac45
fabian <Fabianjose99@gmail.com>
2021-10-12 08:34:54 -0500

    pruebas unitarias (mockmvc, junit, faker) con controlador ok

--- servicio-cliente/src/test/java/com/pragma/serviciocliente/controlador/ClienteControladorTest.java
+++ servicio-cliente/src/test/java/com/pragma/serviciocliente/controlador/ClienteControladorTest.java
@@ -3,10 +3,7 @@ package com.pragma.serviciocliente.controlador;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pragma.serviciocliente.dominio.Cliente;
 import com.pragma.serviciocliente.utils.FakerData;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.Matchers.*;
@@ -23,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureMockMvc
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ClienteControladorTest {
 
     @Autowired
@@ -31,17 +28,21 @@ public class ClienteControladorTest {
     @Autowired
     ObjectMapper objectMapper;
 
-     static Cliente cliente=FakerData.getCliente();
+     static Cliente cliente;
 
 
+    @BeforeAll
+    static void setUp() {
+        cliente = FakerData.getCliente();
+    }
 
     @Test
     @Order(1)
     void guardarClienteTest() throws Exception {
         mvc.perform(multipart("/cliente")
-                        .file("fotoFile", "foto".getBytes(StandardCharsets.UTF_8))
-                        .param("clienteJson", objectMapper.writeValueAsString(cliente))
-                ).andExpect(status().is(HttpStatus.CREATED.value()));
+                .file("fotoFile", "foto".getBytes(StandardCharsets.UTF_8))
+                .param("clienteJson", objectMapper.writeValueAsString(cliente))
+        ).andExpect(status().is(HttpStatus.CREATED.value()));
     }
 
     @Test
@@ -56,28 +57,44 @@ public class ClienteControladorTest {
     @Test
     @Order(3)
     void filtroEdad() throws Exception {
-            mvc.perform(get("/cliente/edad/"+cliente.getEdad()+"")
-                            .accept(MediaType.APPLICATION_JSON))
-                    .andExpect(status().is(HttpStatus.FOUND.value()))
-                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-         .andExpect(jsonPath("$.length()", greaterThanOrEqualTo(1)));
+        mvc.perform(get("/cliente/edad/" + cliente.getEdad() + "")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(HttpStatus.FOUND.value()))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.length()", greaterThanOrEqualTo(1)));
     }
 
     @Test
     @Order(4)
     void filtroId() throws Exception {
-        mvc.perform(get("/cliente/id/"+cliente.getTipoId()+"/"+cliente.getNumeroId()+"")
+        mvc.perform(get("/cliente/id/" + cliente.getTipoId() + "/" + cliente.getNumeroId() + "")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().is(HttpStatus.FOUND.value()))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.nombres").value(cliente.getNombres()));
     }
 
-    /* @Test
+    @Test
     @Order(5)
     void eliminarCliente() throws Exception {
-        mvc.perform(delete("/cliente/id/"+cliente.getTipoId()+"/"+cliente.getNumeroId()+""))
+        mvc.perform(delete("/cliente/id/" + cliente.getTipoId() + "/" + cliente.getNumeroId() + ""))
                 .andExpect(status().is(HttpStatus.OK.value()));
-    } */
+    }
+
+    @Test
+    @Order(6)
+    void filtroIdThrows404() throws Exception {
+        mvc.perform(get("/cliente/id/" + cliente.getTipoId() + "/" + cliente.getNumeroId() + "")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(HttpStatus.NOT_FOUND.value()));
+    }
+
+    @Test
+    @Order(7)
+    void filtroEdadThrows404() throws Exception {
+        mvc.perform(get("/cliente/edad/" + cliente.getEdad() + "")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(HttpStatus.NOT_FOUND.value()));
+    }
 
 }
